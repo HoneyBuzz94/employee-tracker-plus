a2e6c847721a902e4c0c6272850cd5afe88cf333
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-01-21 16:51:12 -0700

    Reworked inquirer prompts to include multiple cases

---
+++ README.md
@@ -0,0 +1,3 @@
+https://github.com/dopecello/employee-tracker/blob/main/lib/menu/menu.js
+https://www.npmjs.com/package/inquirer/v/8.2.4#reactive
+https://www.npmjs.com/package/mysql2
--- index.js
+++ index.js
@@ -3,14 +3,38 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 // General variables
+const questions = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all employees',
+            'Add employee',
+            'Update employee role',
+            'View all roles',
+            'Add role',
+            'View all departments',
+            'Add department',
+            'Quit'
+        ],
+        name: 'menu'
+    }
+];
 
-inquirer.prompt(
-    [
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: ['View all employees','Add employee','Update employee role','View all roles','Add role','View all departments','Add department','Quit'],
-            name: 'mainMenu'
-        }
-    ]
-).then((responses) => console.log(responses));
+function startMenu() {
+    inquirer.prompt(questions).then(({ menu }) => {
+        console.log(menu);
+        switch(menu){
+            case 'View all employees':
+                console.clear();
+                console.log('Here is a list of all employees');
+                startMenu();
+                break;
+            case 'Quit':
+                console.clear();
+                console.log('Thank you for using Employee Tracker Plus!')
+        };
+    });
+};
+
+startMenu();
