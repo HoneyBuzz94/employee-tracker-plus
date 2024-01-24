3f884c31b0d8ad6203a1e50cc1b0cd5acff6fe43
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-01-23 21:34:05 -0700

    Refactored codebase to have functions separated into their own js files

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-// Imports
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const db = require('./config/connection');
 const cTable = require('console.table');
 require('dotenv').config();
+const {viewDepartments, addDepartment} = require('./lib/department');
+const {viewRoles, addRole} = require('./lib/role');
+const {viewEmployees, addEmployee, updateEmployee} = require('./lib/employee');
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    }
-);
-db.connect(function (err) {if(err) throw err;});
-
-// General variables
-const menuOptions = [
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-            'View all employees',
-            'Add employee',
-            'Update employee role',
-            'View all roles',
-            'Add role',
-            'View all departments',
-            'Add department',
-            'Quit'
-        ],
-        name: 'options'
-    }
-];
+const menuOptions = [{
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+        'View all employees',
+        'Add employee',
+        'Update employee role',
+        'View all roles',
+        'Add role',
+        'View all departments',
+        'Add department',
+        'Quit'
+    ],
+    name: 'options'
+}];
 
 function startMenu() {
     inquirer.prompt(menuOptions).then(({ options }) => {
@@ -64,170 +52,6 @@ function startMenu() {
                 db.end();
         };
     });
-};
-
-function viewEmployees(callback) {
-    db.promise().query('SELECT employee.id, employee.first_name, employee.last_name, job.title, department.department_name, job.salary, employee.manager_id FROM employee JOIN job ON employee.job_id = job.id JOIN department ON job.department_id = department.id ORDER BY employee.id').then(([rows]) => {
-        console.clear();
-        console.table(rows);
-        callback();
-    }).catch(err => console.error(err));
-};
-
-function addEmployee(callback) {
-    let roleTitles = [];
-    let employeeNames = [];
-    const questions = [
-        {
-            type: 'input',
-            message: 'First name:',
-            name: 'first_name'
-        },
-        {
-            type: 'input',
-            message: 'Last name:',
-            name: 'last_name'
-        },
-        {
-            type: 'list',
-            message: 'Role:',
-            choices: roleTitles,
-            name: 'role'
-        },
-        {
-            type: 'list',
-            message: 'Manager:',
-            choices: employeeNames,
-            name: 'manager'
-        }
-    ];
-
-    db.promise().query('SELECT * FROM employee JOIN job ON employee.job_id = job.id')
-    .then(([rows]) => {
-        rows.forEach(row => {
-            employeeNames.push(`${row.first_name} ${row.last_name}`);
-            roleTitles.push(row.title);
-        });
-        inquirer.prompt(questions).then((answers) => {
-            const roleID = rows.find(role => role.title == answers.role).id;
-            const managerID = rows.find(employee => `${employee.first_name} ${employee.last_name}` == answers.manager).id;
-
-            db.promise().query(`INSERT INTO employee (first_name, last_name, job_id, manager_id) VALUES ("${answers.first_name}", "${answers.last_name}", ${roleID}, ${managerID})`).then(([rows]) => {
-                console.clear();
-                console.log(`Successfully added new employee.`);
-                callback();
-            });
-        });
-    }).catch(err => console.error(err));
-};
-
-function updateEmployee(callback) {
-    let roleTitles = [];
-    let employeeNames = [];
-    const questions = [
-        {
-            type: 'list',
-            message: 'Employee:',
-            choices: employeeNames,
-            name: 'employee'
-        },
-        {
-            type: 'list',
-            message: 'New role:',
-            choices: roleTitles,
-            name: 'role'
-        },
-    ];
-
-    db.promise().query('SELECT * FROM employee JOIN job ON employee.job_id = job.id')
-    .then(([rows]) => {
-        rows.forEach(row => {
-            employeeNames.push(`${row.first_name} ${row.last_name}`);
-            roleTitles.push(row.title);
-        });
-        inquirer.prompt(questions).then((answers) => {
-            const roleID = rows.find(role => role.title == answers.role).id;
-            const employeeID = rows.find(employee => `${employee.first_name} ${employee.last_name}` == answers.employee).id;
-
-            db.promise().query(`UPDATE employee SET job_id = ${roleID} WHERE employee.id = ${employeeID}`).then(([rows]) => {
-                console.clear();
-                console.log(`Successfully updated employee.`);
-                callback();
-            });
-        });
-    }).catch(err => console.error(err));
-};
-
-function viewRoles(callback) {
-    db.promise().query('SELECT job.id, job.title, job.salary, department.department_name FROM job JOIN department ON job.department_id = department.id ORDER BY job.id').then(([rows]) => {
-        console.clear();
-        console.table(rows);
-        callback();
-    }).catch(err => console.error(err));
-};
-
-function addRole(callback) {
-    let departmentNames = [];
-    const questions = [
-        {
-            type: 'input',
-            message: 'Job title:',
-            name: 'title'
-        },
-        {
-            type: 'input',
-            message: 'Salary:',
-            name: 'salary'
-        },
-        {
-            type: 'list',
-            message: 'Department:',
-            choices: departmentNames,
-            name: 'department'
-        },
-    ];
-
-    db.promise().query('SELECT * FROM job JOIN department ON job.department_id = department.id')
-    .then(([rows]) => {
-        rows.forEach(row => {
-            departmentNames.push(row.department_name);
-        });
-        inquirer.prompt(questions).then((answers) => {
-            const departmentID = rows.find(role => role.title == answers.role).id;
-
-            db.promise().query(`INSERT INTO job (title, salary, department_id) VALUES ("${answers.title}", ${answers.salary}, ${departmentID})`).then(([rows]) => {
-                console.clear();
-                console.log(`Successfully added new role.`);
-                callback();
-            });
-        });
-    }).catch(err => console.error(err));
-};
-
-function viewDepartments(callback) {
-    db.promise().query('SELECT * FROM department ORDER BY department.id').then(([rows]) => {
-        console.clear();
-        console.table(rows);
-        callback();
-    }).catch(err => console.error(err));
-};
-
-function addDepartment(callback) {
-    const questions = [
-        {
-            type: 'input',
-            message: 'Department name:',
-            name: 'department_name'
-        },
-    ];
-
-    inquirer.prompt(questions).then((answers) => {
-        db.promise().query(`INSERT INTO department (department_name) VALUES ("${answers.department_name}")`).then(([rows]) => {
-            console.clear();
-            console.log(`Successfully added new department.`);
-            callback();
-        });
-    }).catch(err => console.error(err));
 };
 
 startMenu();
